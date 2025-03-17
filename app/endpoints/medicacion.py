3141942b241b5f346dccd076f5e8150bba07b552
isagalmar <isagalmar@alum.us.es>
2025-03-17 03:09:47 +0100

    Accepts AI calls

--- app/endpoints/medicacion.py
+++ app/endpoints/medicacion.py
@@ -3,23 +3,15 @@ import datetime
 from fastapi import routing
 
 
-class Dosis():
-
-    def __init__(self, dosis_str:str):
-        cantidad,tamaño = dosis_str.split(" ")
-        
-        self.cantidad = float(cantidad)
-        self.tamaño = str(tamaño)
-
 
 class Medicacion():
     def __init__(self, pacienteId, fecha_inicio, fecha_fin, codigo, nombre, dosis, frecuencia, via):
         self.pacienteId = int(pacienteId)
         self.fecha_inicio = datetime.datetime.fromisoformat(fecha_inicio)
-        self.fecha_fin = datetime.datetime.fromisoformat(fecha_fin)
+        self.fecha_fin = None if fecha_fin!=fecha_fin else datetime.datetime.fromisoformat(fecha_fin)
         self.codigo = codigo
         self.nombre = nombre
-        self.dosis = Dosis(dosis)
+        self.dosis = str(dosis)
         self.frecuencia = str(frecuencia)
         self.via = str(via)
 
